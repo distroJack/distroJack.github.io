62783a8d030a18c9a1b4aa3494cfe39410933bba
Jack Humphries <jswim26@gmail.com>
2024-08-15 00:48:46 -0700

    fix static route generation

--- svelte.config.js
+++ svelte.config.js
@@ -6,15 +6,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true
-		})
+		adapter: adapter()
 	}
 };
 
