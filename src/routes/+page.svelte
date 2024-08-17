e41540c509edfe9e303950de80b0046c768b1952
Jack Humphries <jswim26@gmail.com>
2024-08-16 20:17:40 -0700

    randomize favicons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,23 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <p>what about this thing?</p>
+
+<svelte:head>
+    <meta charset="utf-8">
+    <link id="favicon" rel="icon" href='data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">{emoji}</text></svg>'>
+</svelte:head>
+
+<script lang="ts">
+	import { onMount } from "svelte";
+
+    let emoji: String;
+
+    onMount(() => {
+        let icons = [
+            "🤖", "👋",  "🧙", "👑", 
+            "🏊", "💪🏼", "🐋", "🦈", "🐲", 
+            "🍔", "🎮", "🎰", "🎲", "⚓️", "🃏"
+        ];
+        emoji = icons[Math.floor(Math.random()*icons.length)];
+    });
+</script>
